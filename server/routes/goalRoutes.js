3e351ef6c9d5a15760a1d4c945d4bc9c2907548d
Jul Guga <mr.guga89@hotmail.com>
2022-08-10 17:33:29 +0200

    REST API for users

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const {
   getGoals,
   setGoal,
@@ -7,7 +8,9 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
